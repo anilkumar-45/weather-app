98328ccd377494871e97196e6cc8c509b939ba17
anilkumar-ak2 <121780960+anilkumar-ak2@users.noreply.github.com>
2024-12-30 01:46:18 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import Weather from './components/weather/Weather'
+
+
+export default function App() {
+  return (
+    <div className='App'>
+    <Weather />
+    </div>
+  )
+}
